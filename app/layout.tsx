f54348925bfa26cfb9ba8b056b64746958273dc0
seymademirhikayeleri <seyma.ibrahim.demir@gmail.com>
2026-02-25 02:58:03 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,9 @@
 import "../styles/globals.css"
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: any) {
   return (
     <html lang="tr">
-      <body>
-        <div className="max-w-4xl mx-auto p-6">
-          {children}
-        </div>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
